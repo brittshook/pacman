da84e21a63b84ffcdbd3a45948f4cd25279e3829
brittshook <122318069+brittshook@users.noreply.github.com>
2023-09-07 21:28:14 -0400

    removed unused files and commented out code

--- modules/pellet.js
+++ modules/pellet.js
@@ -1,5 +1,5 @@
 import { context } from './canvasSetup.js';
-import { deg90, deg180, deg270 } from './math.js';
+import { deg180 } from './math.js';
 import Wall from './wall.js';
 //import { redPath, orangePath, yellowPath, greenPath, cyanPath, bluePath, continuousPath, segmentedPath } from './pelletHelperFunctions.js';
 
@@ -16,14 +16,12 @@ export default class Pellet {
 
     draw() {
         const scaleFactor = Pellet.width / 16;
-        
         const centerX = (5 * scaleFactor) + this.position.x;
         const centerY = (5 * scaleFactor) + this.position.y;
         const outerRadius = 5 * scaleFactor;
         const innerRadius = 3 * scaleFactor;
-        
 
-        // Start drawing the first arc
+        // Outer arc
         context.beginPath();
         context.arc(centerX, centerY, outerRadius, deg180, 0);
         context.strokeStyle = '#F96359';
@@ -31,20 +29,12 @@ export default class Pellet {
         context.stroke();
         context.closePath();
 
-
-        // Start drawing the second arc
+        // Inner arc
         context.beginPath();
         context.arc(centerX, centerY, innerRadius, deg180, 0);
         context.strokeStyle = '#F9E759';
         context.lineWidth = 2 * scaleFactor;
         context.stroke();
         context.closePath();
-
-        /*continuousPath(this.position, redPath, '#DC3834', scaleFactor);
-        segmentedPath(this.position, orangePath, '#EE963E', scaleFactor);
-        segmentedPath(this.position, yellowPath, '#F9E759', scaleFactor);
-        continuousPath(this.position, greenPath, '#60B157', scaleFactor);
-        segmentedPath(this.position, cyanPath, '#72F9FC', scaleFactor);
-        continuousPath(this.position, bluePath, '#4A99D3', scaleFactor);*/
     }
 }
